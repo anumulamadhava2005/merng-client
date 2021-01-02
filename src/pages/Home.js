0e9fc4cf5586616239931bef2d7a718f7b17bf42
unknown <manimadhava7@gmail.com>
2021-01-02 13:20:29 +0530

    fix of name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ function Home() {
 const { user } = useContext(AuthContext)
     const {
         loading,
-        data: { getPosts: posts }
+        data: { getPosts: post }
     } = useQuery(FETCH_POSTS_QUERY);
 
     return (
@@ -29,8 +29,8 @@ const { user } = useContext(AuthContext)
                     <h1>Loading posts..</h1>
                 ) : (
                     <Transition.Group>
-                            {posts &&
-                                posts.map((post) => (
+                            {post &&
+                                post.map((post) => (
                                     <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
                                         <PostCard post={post} />
                                     </Grid.Column>
