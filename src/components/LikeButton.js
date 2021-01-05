c8c3dbe73cfaa376a7ac5eefff8486db3f802fe2
anumulamadhava2005 <49472497+anumulamadhava2005@users.noreply.github.com>
2021-01-05 14:06:44 +0530

    Update LikeButton.js

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -20,7 +20,7 @@ function LikeButton({post: { id, likeCount, likes }, user }) {
 
     const likeButton = user ? (
         liked ? (
-            <Button color='pink'  >
+            <Button basic color='pink'  >
                     <Icon name='heart' />
             </Button>
         ) : (
