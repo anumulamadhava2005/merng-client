e8ce5f6b54ecadb47b54b90000690fc585bd7988
anumulamadhava2005 <49472497+anumulamadhava2005@users.noreply.github.com>
2021-01-05 13:47:47 +0530

    Update PostForm.js

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -56,7 +56,7 @@ function PostForm(){
    // update width and height ...
  }
  <input type='file' />
- <br>< img id="myImg" src="#" alt="your image" height=300 width='400'>
+ <br>< img id="myImg" src="#" alt="your image" height="300" width="400">
      </img>
      </br>
                  </Form.Field>
