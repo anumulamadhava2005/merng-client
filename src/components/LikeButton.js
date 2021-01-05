41907c7174b9b6b79ffb4b5b8dc4d6b079b4e239
anumulamadhava2005 <49472497+anumulamadhava2005@users.noreply.github.com>
2021-01-05 09:57:51 +0530

    Update LikeButton.js

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -41,7 +41,7 @@ function LikeButton({post: { id, likeCount, likes }, user }) {
             trigger={
                 <Button as="div" labelPosition='left' onClick={likePost}>
                     {likeButton}
-                    <Label basic color='blue' pointing='left'>
+                    <Label basic color='blue' pointing='left' style={{marginright: 20}}>
                         {likeCount}
                     </Label>
                 </Button>
