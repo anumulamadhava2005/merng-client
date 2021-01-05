a79ae1766b42c336c0123e3fa7a8db24986beaba
anumulamadhava2005 <49472497+anumulamadhava2005@users.noreply.github.com>
2021-01-05 09:42:40 +0530

    Update MenuBar.js

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -13,7 +13,7 @@ function MenuBar () {
     const handleItemClick = (e, { name }) => setActiveItem(name);
 
     const menuBar = user ? (
-        <Menu pointing secondary size="huge" color="pink" >
+        <Menu pointing secondary size="huge" color="blue" >
                     <Menu.Item
                         name={user.username}
                         active
