706d78dd0ed80a4107204445690cd17335800367
anumulamadhava2005 <49472497+anumulamadhava2005@users.noreply.github.com>
2021-01-05 12:43:14 +0530

    Update PostForm.js

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -41,6 +41,22 @@ function PostForm(){
                     <Button type="submit" color="orange">
                         Commit
                 </Button>
+        window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');  // $('img')[0]
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+          img.onload = imageIsLoaded;
+      }
+  });
+});
+
+function imageIsLoaded() { 
+  alert(this.src);  // blob url
+  // update width and height ...
+}
+<input type='file' />
+<br><img id="myImg" src="#" alt="your image" height=200 width=100>
                 </Form.Field>
             </Form>
             {error && (
